138cfd9c9062ebbfc4e6e903c0cd852afa24056e
ZHANGZHENG <sclzzzheng@163.com>
2020-06-27 17:23:33 +0800

    添加一些注释和配置，增加README.txt文件说明

--- common-utils/src/main/java/com/ca/common/utils/CommonUtilsApplication.java
+++ common-utils/src/main/java/com/ca/common/utils/CommonUtilsApplication.java
@@ -3,6 +3,15 @@ package com.ca.common.utils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * forum-common
+ *
+ * <p> common启动类 </p>
+ *
+ * @author zz
+ *
+ * @version CommonUtilsApplication.java 2020/6/27
+ **/
 @SpringBootApplication
 public class CommonUtilsApplication {
 
